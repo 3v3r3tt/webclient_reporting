cfa3cc5c8a4543ff64c3ff1ad23c2165ca8f8d3c
Benton Rochester <bentonrochester@gmail.com>
2017-09-21 15:29:33 -0600

    Feat/pbj 193 populating initial data for prod (#1497)
    * PBJ-193 adding production version of webpack config
    
    * feat/PBJ-193 populating initialState and api

--- store/configureStore.production.js
+++ store/configureStore.production.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from '../reducers/index'
+import { api } from 'components/__utils/xhr'
+import createSagas from '../sagas'
 
 const sagaMiddleware = createSagaMiddleware()
 
@@ -8,6 +10,13 @@ const finalCreateStore = compose(
   applyMiddleware(sagaMiddleware)
 )(createStore)
 
-export default function configureStore (initialState) {
-  return finalCreateStore(rootReducer, initialState)
+export default function configureStore (initialData) {
+  const voStore = finalCreateStore(
+    rootReducer,
+    initialData
+  )
+
+  sagaMiddleware.run(createSagas(api))
+
+  return voStore
 }
