3c275d69ba03d96e75b14bbb7475faabf8becd00
Benton Rochester <bentonrochester@gmail.com>
2017-10-26 14:22:56 -0600

    PBJ-313 – made modals consistent (#1659)

--- components/iframe-profile/iframe-modal.js
+++ components/iframe-profile/iframe-modal.js
@@ -6,15 +6,18 @@ function iframeModal (props) {
   const location = `/dash/${org}/?iframe#profile`
   const username = config.getIn(['auth', 'user', 'username'], '')
   return (
-    <div className='js-modal-profile vo-modal vo-modal-profile vo-modal-full vo-modal-iframe fade in' tabIndex='-1' role='dialog' style={{'zIndex': '10000'}}>
-      <div>
-        <div className='js-profile-header profile-header'>
-          <div className='shadow-cover'>
-            <span className='icon-remove' data-dismiss='modal' onClick={() => props.modalActions.hideModal()} aria-hidden='true' title='Click or [esc] to close' />
-            <h4><span className='subtitle'>Profile:</span> @{username}</h4>
+    <div>
+      <div className='modal-backdrop fade-in' />
+      <div className='js-modal-profile vo-modal vo-modal-profile vo-modal-full vo-modal-iframe fade in' tabIndex='-1' role='dialog' style={{'zIndex': '10000'}}>
+        <div>
+          <div className='js-profile-header profile-header'>
+            <div className='shadow-cover'>
+              <span className='icon-remove' data-dismiss='modal' onClick={() => props.modalActions.hideModal()} aria-hidden='true' title='Click or [esc] to close' />
+              <h4><span className='subtitle'>Profile:</span> @{username}</h4>
+            </div>
           </div>
+          <iframe scrolling='no' className='js-profile-iframe' src={location} style={{'height': '580px'}} />
         </div>
-        <iframe scrolling='no' className='js-profile-iframe' src={location} style={{'height': '580px'}} />
       </div>
     </div>
   )
