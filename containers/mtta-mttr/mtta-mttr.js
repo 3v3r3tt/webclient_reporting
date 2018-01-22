e2a652ad859b4c57a26895ba18cc533af07fecb0
Paul Morgan <3v3r3tt@users.noreply.github.com>
2018-01-22 10:21:33 -0700

    PBJ-671: MMR: Capitalize reports in header (#2055)

--- containers/mtta-mttr/mtta-mttr.js
+++ containers/mtta-mttr/mtta-mttr.js
@@ -28,8 +28,6 @@ function mapDispatchToProps (dispatch) {
 
 class MttaMttr extends Component {
   render () {
-    console.log('MttaMttr RENDER.... this.props: ', this.props)
-
     const ReportsLink = <Link className='link--default' to={`/reports/${config.orgslug}`}>Reports</Link>
     return (
       <div className='container module-wrapper'>
@@ -38,7 +36,7 @@ class MttaMttr extends Component {
           {label: 'Performance (MTTA/MTTR) ', uri: 'reports/mtta-ttr', active: true}
         ]} light />
 
-        <h1 className='heading-3'>Performance (MTTA/MTTR) report</h1>
+        <h1 className='heading-3'>Performance (MTTA/MTTR) Report</h1>
 
         <Filter />
 
