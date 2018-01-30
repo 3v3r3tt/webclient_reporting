9fb46eebf97de52ff236ff09bbf8e42d70541793
Brandon Zarrella <zarrellab@users.noreply.github.com>
2018-01-29 18:16:11 -0700

    Activate Intercom (#2106)
    * remove qualifiers
    
    * default to hide for paid users
    
    * rm olark
    
    * default hide intercom on timeline or for paid users
    
    * rm styles for olark and ramen
    
    * fix date to intercom's standards
    
    * bump launcher above footer

--- auth.js
+++ auth.js
@@ -2,4 +2,4 @@ import appInit from 'util/appInit'
 
 var orgSlug = /\/reports\/(.*)/.exec(window.location)
 const startApp = () => require('./app').default()
-appInit(orgSlug, startApp, 'Reporting', true /* temp until olark is gone */)
+appInit(orgSlug, startApp, 'Reporting')
